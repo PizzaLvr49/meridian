6e70caa2cf24c0509d17f52c6aac20288f4cbc50
ExoticSliceOfPizza <136937744+PizzaLvr49@users.noreply.github.com>
2025-10-30 21:45:27 -0400

    stuff

--- crates/meridian_client/src/main.rs
+++ crates/meridian_client/src/main.rs
@@ -3,7 +3,7 @@
 use bevy::prelude::*;
 use bevy::window::{PresentMode, WindowMode};
 use bevy_panic::PanicHandler;
-use meridian_assets_handler::TilePlugin;
+use meridian_assets_handler::{AssetLoadingState, AssetPlugin, ImageAssets, PlayerAsset};
 
 fn main() {
     App::new()
@@ -17,6 +17,21 @@ fn main() {
             ..default()
         }))
         .add_plugins(PanicHandler::default())
-        .add_plugins(TilePlugin)
+        .add_plugins(AssetPlugin)
+        .add_systems(OnEnter(AssetLoadingState::Finished), spawn_player)
         .run();
+}
+
+fn spawn_player(
+    mut commands: Commands,
+    image_assets: Res<ImageAssets>,
+    player_assets: Res<Assets<PlayerAsset>>,
+) {
+    if let Some(player_asset) = player_assets.get(&image_assets.player) {
+        commands.spawn(Sprite::from_image(player_asset.image.clone()));
+
+        println!("Player name: {}", player_asset.metadata.name);
+        println!("Player health: {}", player_asset.metadata.health);
+        println!("Player speed: {}", player_asset.metadata.speed);
+    }
 }
